7f857a6a88a50c9967b3c8a069a33c007bf35691
Cancerous115 <107626947+Cancerous115@users.noreply.github.com>
2023-02-26 22:58:01 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    int number = 15;
+    int number2 = 5;
+
+    // Greater than
+    System.out.println(number > number2);
+    // less than
+    System.out.println(number < number2);
+    // not equal
+    System.out.println(number != number2);
+    // less than or equal
+    System.out.println(number <= number2);
+    // greater than or equal
+    System.out.println(number >= number2);
+  }
+}
